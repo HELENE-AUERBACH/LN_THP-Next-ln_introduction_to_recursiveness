8d821ce3d1f9e11ab8dc872eb17bde4131cc0921
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-07-12 19:43:14 +0200

    Addition of the "findSupPrime" function.

--- prime_number.js
+++ prime_number.js
@@ -35,12 +35,36 @@ const isPrimeNumber = (n) => {
   return result;
 }
 
+console.log("isPrimeNumber(0) = ", isPrimeNumber(0)); // output : false
 console.log("isPrimeNumber(1) = ", isPrimeNumber(1)); // output : false
 console.log("isPrimeNumber(2) = ", isPrimeNumber(2)); // output : true
 console.log("isPrimeNumber(3) = ", isPrimeNumber(3)); // output : true
 console.log("isPrimeNumber(6) = ", isPrimeNumber(6)); // output : false
 console.log("isPrimeNumber(7) = ", isPrimeNumber(7)); // output : true
 console.log("isPrimeNumber(41) = ", isPrimeNumber(41)); // output : true
-console.log("isPrimeNumber(\"3\") = ", isPrimeNumber("3")); // output : false
-console.log("isPrimeNumber({}) = ", isPrimeNumber({})); // output : false
-console.log("isPrimeNumber(-3) = ", isPrimeNumber(-3)); // output : false
+console.log("isPrimeNumber(\"3\") = ", isPrimeNumber("3")); // output : false <=> error
+console.log("isPrimeNumber({}) = ", isPrimeNumber({})); // output : false <=> error
+console.log("isPrimeNumber(-3) = ", isPrimeNumber(-3)); // output : false <=> error
+
+/* findSupPrime returns the littlest prime number >= n. */
+const findSupPrime = (n) => {
+  let result = 0;
+  if (n !== undefined && n !== null && (typeof n === 'number') && !isNaN(n) && Number.parseInt(n) == Number.parseFloat(n)) {
+    if (isPrimeNumber(n)) {
+      result = n;
+    } else {
+      result = findSupPrime(n + 1);
+    }
+  }
+  return result;
+}
+
+console.log("findSupPrime(0) = ", findSupPrime(0)); // output : 2
+console.log("findSupPrime(1) = ", findSupPrime(1)); // output : 2
+console.log("findSupPrime(2) = ", findSupPrime(2)); // output : 2
+console.log("findSupPrime(3) = ", findSupPrime(3)); // output : 3
+console.log("findSupPrime(6) = ", findSupPrime(6)); // output : 7
+console.log("findSupPrime(7) = ", findSupPrime(7)); // output : 7
+console.log("findSupPrime(\"3\") = ", findSupPrime("3")); // output : 0 <=> error
+console.log("findSupPrime({}) = ", findSupPrime({})); // output : 0 <=> error
+console.log("findSupPrime(-3) = ", findSupPrime(-3)); // output : 2
